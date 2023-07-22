feba70284fe06fc7f3252bd337d94ad7ff5eff70
Monishka Jha <jhamonishka@gmail.com>
2023-07-22 20:20:14 +0530

    Day 3

---
+++ Learn/Patterns.cpp
@@ -0,0 +1,177 @@
+//Pattern 1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        
+        // code here
+        for(int i =0; i < n; i++){
+            for(int j =0; j < n; j++){
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+        
+    }
+};
+
+
+// Pattern 2
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i =0; i< n; i++){
+            for(int j = 0; j<=i; j++){
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Pattern 3
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i = 0; i <=n; i++){
+            for(int j = 1; j<=i; j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Pattern 4
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i =1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                cout<<i<<" ";
+            }
+            
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Pattern 5
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    for(int i = 0; i <n; i++){
+	        for(int j = n-i; j>0; j--){
+	            cout<<"*"<<" ";
+	        }
+	        cout<<endl;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Pattern 6
+
+
+
