b6009d1e16f349fae6e5577dbaffe4bc59eced3d
Monishka Jha <jhamonishka@gmail.com>
2023-07-25 00:00:57 +0530

    Day 4

--- Learn/Patterns.cpp
+++ Learn/Patterns.cpp
@@ -173,5 +173,194 @@ int main() {
 
 // Pattern 6
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j = 0; j < n-i-1; j++){
+                cout<<" ";
+            }
+            for(int j =0; j < 2*i + 1; j++){
+                cout<<"*";
+            }
+            for(int j = 0; j < n-i-1; j++){
+                cout<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+// Pattern 7
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code heren-
+	    for(int i =0; i<n; i++){
+	        for(int j = 0; j < i;j++){
+	            cout<<" ";
+	        }
+	        for(int j = 0; j < 2*n -(2*i + 1); j++){
+	            cout<<"*";
+	        }
+	        for(int j = 0; j < i;j++){
+	            cout<<" ";
+	        }
+	        
+	        cout<<endl;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+//Pattern 8
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//Back-end complete function Template for C++
+class Solution{
+public:
+    void printTriangle(int n) {
+        // code here\for()
+        for(int i = 1; i<=2*n-1; i++){
+            int stars = i;
+            if(i>n) stars = 2*n -i;
+            for(int j = 1; j<= stars; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+//Pattern 9
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int N) {
+        // code here
+        int spaces = 2*(N-1);
+      
+      // Outer loop for the number of rows.
+      for(int i=1;i<=N;i++){
+          
+          // for printing numbers in each row
+          for(int j=1;j<=i;j++){
+              cout<<j<<" ";
+          }
+          
+          // for printing spaces in each row
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          
+          // for printing numbers in each row
+          for(int j=i;j>=1;j--){
+              cout<<j<<" ";
+          }
+          
+          // As soon as the numbers for each iteration are printed, we move to the
+          // next row and give a line break otherwise all numbers
+          // would get printed in 1 line.
+          cout<<endl;
+          
+          // After each iteration nos. increase by 2, thus
+          // spaces will decrement by 2.
+          spaces-=2;
+          
+      }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+//Pattern 10
 
 
